e9a48876260afc44332960efb10b496da90e9437
Emilio Loi <loi.emilio@gmail.com>
2018-07-26 20:40:10 +0100

    Added gitignore file

--- .dep/obj_hbkb_lufa_common_print.o.d
+++
@@ -1,8 +0,0 @@
-obj_hbkb_lufa/common/print.o: ../../tmk_core/common/print.c config.h \
- ../../tmk_core/common/print.h ../../tmk_core/common/util.h
-
-config.h:
-
-../../tmk_core/common/print.h:
-
-../../tmk_core/common/util.h:
--- .dep/obj_hbkb_lufa_led.o.d
+++
@@ -1,5 +0,0 @@
-obj_hbkb_lufa/led.o: led.c config.h ../../tmk_core/common/led.h
-
-config.h:
-
-../../tmk_core/common/led.h:
